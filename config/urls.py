04db6af506f44db266ad17c5112a4eb251a4148f
kierudsen <tebrasso@gmail.com>
2019-06-18 14:49:47 +0200

    another url-setting

--- config/urls.py
+++ config/urls.py
@@ -1,64 +1,33 @@
-from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.contrib import admin
-from django.views import defaults as default_views
+from django.conf import settings
+import os.path
 
-# Wagtail imports
-from django.views.generic import TemplateView
+from wagtail.core import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from wagtail.core import urls as wagtail_urls
+
 
 urlpatterns = [
-    url("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    url(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    url(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    # path("users/", include("kierudsen.users.urls", namespace="users")),
-    # path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
-    # wagtail URL conf
-    # re_path(r'^cms/', include(wagtailadmin_urls)),
-    # re_path(r'^documents/', include(wagtaildocs_urls)),
-    # re_path(r'^pages/', include(wagtail_urls)),
-    # url(r'^django-admin/', (admin.site.urls)),
-    url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^pages/', include(wagtail_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^django-admin/', include(admin.site.urls)),
 
-urlpatterns += [
-    url(r'', include(wagtail_urls))
+    re_path(r'^admin/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+
+    # For anything not caught by a more specific rule above, hand over to
+    # Wagtail's serving mechanism
+    re_path(r'', include(wagtail_urls)),
 ]
 
+
 if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
+    urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
     urlpatterns += [
-        url(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        url(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        url(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        url("500/", default_views.server_error),
+        re_path(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'myapp/images/favicon.ico'))
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
-
-        urlpatterns = [url("__debug__/", include(debug_toolbar.urls))] + urlpatterns
-
 
