7240ae453dd4363f2b27d68e5c9a31d498cd4ddb
kierudsen <tebrasso@gmail.com>
2019-06-18 13:29:37 +0200

    some cleanup

--- kierudsen/users/models.py
+++ kierudsen/users/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+class User(AbstractUser):
+    pass
 
 class HomePage(Page):
     body = RichTextField(blank=True)
