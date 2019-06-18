ea707d99607196163b1ea85a7c023ebd894393bd
kierudsen <tebrasso@gmail.com>
2019-06-18 13:42:01 +0200

    fixing urls

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 # Wagtail imports
@@ -12,21 +10,21 @@ from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
-    #path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    #path(
+    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    # path(
     #    "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    #),
+    # ),
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
     # User management
-    #path("users/", include("kierudsen.users.urls", namespace="users")),
-    #path("accounts/", include("allauth.urls")),
+    # path("users/", include("kierudsen.users.urls", namespace="users")),
+    # path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     # wagtail URL conf
-    #re_path(r'^cms/', include(wagtailadmin_urls)),
-    #re_path(r'^documents/', include(wagtaildocs_urls)),
-    #re_path(r'^pages/', include(wagtail_urls)),
-    url(r'^django-admin/', (admin.site.urls)),
+    # re_path(r'^cms/', include(wagtailadmin_urls)),
+    # re_path(r'^documents/', include(wagtaildocs_urls)),
+    # re_path(r'^pages/', include(wagtail_urls)),
+    # url(r'^django-admin/', (admin.site.urls)),
     url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^pages/', include(wagtail_urls)),
@@ -40,26 +38,26 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        path(
+        url(
             "400/",
             default_views.bad_request,
             kwargs={"exception": Exception("Bad Request!")},
         ),
-        path(
+        url(
             "403/",
             default_views.permission_denied,
             kwargs={"exception": Exception("Permission Denied")},
         ),
-        path(
+        url(
             "404/",
             default_views.page_not_found,
             kwargs={"exception": Exception("Page not Found")},
         ),
-        path("500/", default_views.server_error),
+        url("500/", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [url("__debug__/", include(debug_toolbar.urls))] + urlpatterns
 
 
