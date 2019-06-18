82108cce997ee3b91a3f77084f454dce695fb209
kierudsen <tebrasso@gmail.com>
2019-06-18 19:47:00 +0000

    Site is working:-)

--- kierudsen/users/models.py
+++ kierudsen/users/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.db.models import CharField
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+
 class User(AbstractUser):
-    pass
+
+    name = CharField(_("Name of User"), blank=True, max_length=255)
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
 
 class HomePage(Page):
     body = RichTextField(blank=True)
